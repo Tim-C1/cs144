29bb4a9072fd4ccd9d9c2ebb82ac176b5a616cdf
ysyx_22040343-Chen Tingrun <chentingrun1011@163.com>
2023-03-12 18:46:44 +0800

    lab0

--- apps/webget.cc
+++ apps/webget.cc
@@ -8,7 +8,13 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
-
+    TCPSocket tcp;
+    tcp.connect(Address(host, "http"));
+    tcp.write("GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close" + "\r\n\r\n");
+    while (!tcp.eof()) {
+        cout << tcp.read();
+    }
+    return;
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
